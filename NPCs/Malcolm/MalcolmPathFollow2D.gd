3c112e967994cb5c329a9b0922360be446431d8b
average-noodles <95443534+average-noodles@users.noreply.github.com>
2021-12-03 01:52:07 -0600

    Adding NPCs into game

--- NPCs/Malcolm/MalcolmPathFollow2D.gd
+++ NPCs/Malcolm/MalcolmPathFollow2D.gd
@@ -6,7 +6,7 @@ extends KinematicBody2D
 # var b = "text"
 onready var animation_player = get_node("AnimationPlayer")
 onready var door = get_node("/root/World/MainEnterance")
-onready var path_2d = get_node("/root/World/NPCsPath/VelmiPath2D/PathFollow2D")
+onready var path_2d = get_node("/root/World/NPCsPath/MalcolmPath2D/PathFollow2D")
 export var runSpeed = 20
 var enter = false
 var exit = false
--- World/World.gd
+++ World/World.gd
@@ -6,23 +6,16 @@ extends Node2D
 # var b = "text"
 
 onready var clock = get_node("Clock")
-onready var character_dictionary = {
-	"Velmi": {
-		"time": {
-			"in_time": 500,
-			"out_time": 100000
-		},
-		"final_animation": "IdleLeft",
-		"file_location": "res://NPCs/Velmi/Velmi.tscn",
-		"npc_path": "YSort/VelmiPath2D"
-	}
-}
+onready var game_state_controller = get_node(("/root/GameStateController"))
 onready var player = get_node("YSort/Player")
+onready var ysort = get_node("YSort")
 onready var dialog_player = get_node("Dialog_Player")
 
 var current_npcs = {}
+var character_dictionary = {}
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	character_dictionary = game_state_controller.get_data("npcs")
 	for key in character_dictionary:
 		var time_range = character_dictionary[key]["time"]
 		var time = clock.get_time()
@@ -30,9 +23,14 @@ func _ready():
 		if time_range["in_time"] <= time and time_range["out_time"] > time:
 			var npc_instance = load(character_dictionary[key]["file_location"])
 			var npc = npc_instance.instance()
+			ysort.add_child(npc)
 			var npc_path = get_node(character_dictionary[key]["npc_path"])
-			npc_path.add_child(npc)
-			npc.set_unit_offset(1)
+			var remote_transform = RemoteTransform2D.new()
+			remote_transform.set_update_rotation(false)
+			remote_transform.set_update_scale(false)
+			remote_transform.set_remote_node(character_dictionary[key]["ysort_path"])
+			npc_path.add_child(remote_transform)
+			npc_path.set_unit_offset(1)
 			npc.play_animation(character_dictionary[key]["final_animation"])
 			current_npcs[key] = {"time": character_dictionary[key]["time"], "npc": npc}
 			
@@ -53,8 +51,14 @@ func _process(delta):
 		if (time_range["in_time"] <= time and time_range["out_time"] > time) and not current_npcs.has(key):
 			var npc_instance = load(character_dictionary[key]["file_location"])
 			var npc = npc_instance.instance()
+			ysort.add_child(npc)
 			var npc_path = get_node(character_dictionary[key]["npc_path"])
-			npc_path.add_child(npc)
+			var remote_transform = RemoteTransform2D.new()
+			remote_transform.set_update_rotation(false)
+			remote_transform.set_update_scale(false)
+			remote_transform.set_remote_node(character_dictionary[key]["ysort_path"])
+			npc_path.add_child(remote_transform)
+			npc_path.set_unit_offset(1)
 			npc.set_enter(true)
 			current_npcs[key] = {"time": character_dictionary[key]["time"], "npc": npc}	
 	

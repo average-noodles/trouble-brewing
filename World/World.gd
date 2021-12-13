2fbfaa5d393621dd7eaaaeff739adfaf10779e66
average-noodles <95443534+average-noodles@users.noreply.github.com>
2021-12-12 20:25:48 -0600

    dialog-exploration-patch-part2

--- World/World.gd
+++ World/World.gd
@@ -17,22 +17,23 @@ var character_dictionary = {}
 func _ready():
 	character_dictionary = game_state_controller.get_data("npcs")
 	for key in character_dictionary:
-		var time_range = character_dictionary[key]["time"]
-		var time = clock.get_time()
-		time = time[0] * 60 + time[1]
-		if (time_range["in_time"] <= time and time_range["out_time"] > time) and check_day(character_dictionary[key]["days_in_shop"],clock.get_day()):
-			var npc_instance = load(character_dictionary[key]["file_location"])
-			var npc = npc_instance.instance()
-			ysort.add_child(npc)
-			var npc_path = get_node(character_dictionary[key]["npc_path"])
-			var remote_transform = RemoteTransform2D.new()
-			remote_transform.set_update_rotation(false)
-			remote_transform.set_update_scale(false)
-			remote_transform.set_remote_node(character_dictionary[key]["ysort_path"])
-			npc_path.add_child(remote_transform)
-			npc_path.set_unit_offset(1)
-			npc.play_animation(character_dictionary[key]["final_animation"])
-			current_npcs[key] = {"time": character_dictionary[key]["time"], "npc": npc}
+		if key!="Watcher":
+			var time_range = character_dictionary[key]["time"]
+			var time = clock.get_time()
+			time = time[0] * 60 + time[1]
+			if (time_range["in_time"] <= time and time_range["out_time"] > time) and check_day(character_dictionary[key]["days_in_shop"],clock.get_day()):
+				var npc_instance = load(character_dictionary[key]["file_location"])
+				var npc = npc_instance.instance()
+				ysort.add_child(npc)
+				var npc_path = get_node(character_dictionary[key]["npc_path"])
+				var remote_transform = RemoteTransform2D.new()
+				remote_transform.set_update_rotation(false)
+				remote_transform.set_update_scale(false)
+				remote_transform.set_remote_node(character_dictionary[key]["ysort_path"])
+				npc_path.add_child(remote_transform)
+				npc_path.set_unit_offset(1)
+				npc.play_animation(character_dictionary[key]["final_animation"])
+				current_npcs[key] = {"time": character_dictionary[key]["time"], "npc": npc}
 			
 
 func _process(delta):
